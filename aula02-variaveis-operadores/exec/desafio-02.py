19a3fdb4944b1cdec64c22be40e14b2a3910af42
victoralvao7 <victorgalvao582@gmail.com>
2026-03-16 20:45:51 -0300

    aula 2 variaveis

---
+++ aula02-variaveis-operadores/exec/desafio-02.py
@@ -0,0 +1,4 @@
+dia = int(input("Qual dia você nasceu: "))
+mes = int(input("Qual més você nasceu: "))
+ano = int(input("Qual ano você nasceu: "))
+print(f"Sua data de nascimento formatada seria {dia:0>2}/{mes:0>2}/{ano:0>2}")
